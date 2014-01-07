d838d8ddcfeaac301125b24039ac5435c629a57f
Jeremy Evans <code@jeremyevans.net>
2014-01-07 10:46:44 -0800

    Add thamble/rails for automatic integration with Rails

---
+++ lib/thamble/rails.rb
@@ -0,0 +1,11 @@
+require 'thamble'
+require 'active_support/core_ext/string/output_safety'
+ActiveSupport::SafeBuffer.send(:include, Thamble::Raw)
+
+module Thamble
+  module RailsHelper
+    def thamble(*a, &block)
+      raw Thamble.table(*a, &block)
+    end
+  end
+end
