784b371e7ae493ee8ebb1319c4934d00cbc81e70
Jeremy Evans <code@jeremyevans.net>
2019-05-31 10:14:05 -0700

    Do not load minitest plugins when testing

--- spec/thamble_spec.rb
+++ spec/thamble_spec.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require File.join(File.dirname(File.expand_path(__FILE__)), '../lib/thamble')
+ENV['MT_NO_PLUGINS'] = '1' # Work around stupid autoloading of plugins
 require 'minitest/autorun'
 
 describe "Thamble.table" do
